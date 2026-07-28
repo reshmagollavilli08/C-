f367b4a30b2ebad7b38e2b9e7edc9a4b65a2afa7
reshmagollavilli08 <reshmagollavilli@gmail.com>
2026-07-28 10:39:17 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	cout<<"Enter a value\n";
+	cin>>a;
+	cout<<"Enter b value\n";
+	cin>>b;
+	int result;
+	result=a+b;
+	cout<<"Result="<<result;
+	return 0;
+	
+}
